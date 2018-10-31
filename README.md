1229181b8194c76ee666d742c07c879faa9275be
zhouy_cqfjtech <zhouy@cqfjtech.com>
2018-10-31 15:00:42 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # jpinyin
+
+功能：
+	获取汉字拼音的首字母，支持多音字。
+	
+特点：
+	支持多音字；
+	支持设置特定多音字为固定字母
+	
+使用：
+	result := jpinyin.GetInitiallist("需要获取拼音的字符串")
+	
+作者：
+	周毅(zhoulingamao@163.com)
+
--- main.go
+++ main.go
@@ -9,11 +9,10 @@ import (
 
 func main() {
 
+	//调用GetInitiallist函数获取字符串拼音首字母，返回结果为[]string
 	arrResult := jpinyin.GetInitiallist("a1重庆长壳大桥")
+
 	strResult := strings.Join(arrResult, ",")
-	if len(strResult) > 10 {
-		strResult = strResult[:10]
-	}
 
 	fmt.Println(strResult)
 }
